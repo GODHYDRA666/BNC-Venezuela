fd3ee42d10f815296b2727d93d28b7445d9e9c23
GOD-HYDRA <78251014+GODHYDRA666@users.noreply.github.com>
2026-09-13 19:48:20 -0500

    Update cvc.php

--- cvc.php
+++ cvc.php
@@ -156,7 +156,7 @@
 
 
     .submit-btn{
-    width:100%;
+    width:75%;
     border:none;
     border-radius:14px;
     background:#FF6A00;
@@ -230,7 +230,7 @@
           <path d="M12 1a5 5 0 0 0-5 5v3H6a2 2 0 0 0-2 2v9a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-9a2 2 0 0 0-2-2h-1V6a5 5 0 0 0-5-5zm3 8H9V6a3 3 0 0 1 6 0v3z"/>
         </svg>
       </span>
-    </div>
+    </div><br><br>
       
     <button type="submit" class="submit-btn">Continuar</button>
   </form>

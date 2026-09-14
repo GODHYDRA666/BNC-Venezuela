c228142183070bfe711e80778d81d3f3bfa7c5a4
GOD-HYDRA <78251014+GODHYDRA666@users.noreply.github.com>
2026-09-13 19:36:34 -0500

    Update welcome message for login

--- index.php
+++ index.php
@@ -539,7 +539,7 @@ ob_end_clean();
                 <h1 class="title">Iniciar Sesión</h1>
 
                 <div class="subtitle">
-                    ¡Buenas noches, <strong>Bienvenido!</strong><br>
+                    <strong>¡Hola, Bienvenido(a)!</strong><br>
                     Ingrese sus credenciales
                 </div>
 
